845b0d7cc587db1d4d64514eecdf19566135f554
kelly973 <70935890+kelly973@users.noreply.github.com>
2021-05-27 15:44:07 +0900

    Update app.py

--- action/app.py
+++ action/app.py
@@ -1,13 +1,4 @@
-from confluent_kafka.schema_registry import SchemaRegistryClient # confluent-kafka 라이브러리 활용
-conf = {'url':'http://0.0.0.0:8085'}  # schema registry 호스트 지정
-client = SchemaRegistryClient(conf) # schema registry client 생성
-
-from confluent_kafka.schema_registry import Schema
+import requests
 
 f = open('../sample.proto','r')
-new_str = f.read().__str__()
-    
-new_schema = Schema(new_str, 'PROTOBUF',references=[]) # 신규 스키마 생성
-res = client.register_schema('new_schema_test',new_schema) # 신규 스키마 레지스트리에 등록
-
-print(res)
+print(f.read())
